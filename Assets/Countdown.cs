dd7fe4ecad013ebb5c586bb9a59fd8f35c44e64f
yash <yash.choksey@outlook.com>
2019-10-30 10:16:31 -0400

    ok hope this doesn't break the world

---
+++ Assets/Countdown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class Countdown : MonoBehaviour
+{
+    public int RoundTime= 99;
+    public Text countdown;//the timer gameobject
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("TimegoDown");
+        Time.timeScale=1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       countdown.text=(""+RoundTime); 
+    }
+    IEnumerator TimegoDown()
+    {
+        while(true){
+            yield return new WaitForSeconds(1);
+            RoundTime--;
+        }
+
+    }
+}
---
+++ Assets/gameManager.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class gameManager : MonoBehaviour
+{
+    public float barDisplay; //current progress
+    public Vector2 pos1 = new Vector2(20, 40);
+    public Vector2 size1 = new Vector2(60, 20);
+    public Texture2D emptyTex;
+    public Texture2D fullTex;
+    public GameObject player1;
+    public GameObject player2;
+
+    private void Start()
+    {
+        Player p1=player1.GetComponent<Player>;
+    }
+
+    void OnGUI()
+    {
+        //draw the background:
+        GUI.BeginGroup(new Rect(pos1.x, pos1.y, size1.x, size1.y));
+        GUI.Box(new Rect(0, 0, size1.x, size1.y), emptyTex);
+
+        //draw the filled-in part:
+        GUI.BeginGroup(new Rect(0, 0, size1.x * barDisplay, size1.y));
+        GUI.Box(new Rect(0, 0, size1.x, size1.y), fullTex);
+        GUI.EndGroup();
+        GUI.EndGroup();
+    }
+
+    void Update()
+    {
+        //for this example, the bar display is linked to the current time,
+        //however you would set this value based on your desired display
+        //eg, the loading progress, the player's health, or whatever.
+        barDisplay = player1.health * 0.05f;
+        //        barDisplay = MyControlScript.staticHealth;
+    }
+}
